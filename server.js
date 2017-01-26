d77f9283f63d9c792e0219cfaa7afa0056c63c30
Jonathan <jonmorazav@yahoo.com>
2017-01-26 06:31:10 +0000

    added server logic for registering and logging in using hashing and salting

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const session = require('express-session');
 const mongoClient = require('mongodb').MongoClient;
 const MongoStore = require('connect-mongo')(express);
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
 
 
 const hostname = process.env.IP;
 const port = process.env.PORT;
+const saltRounds = 4;
 
 var userSession;
 
@@ -20,7 +22,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(session({
   secret: 'Orange and red keyboard fish',
   resave: false,
-  store: new MongoStore({ url: 'mongodb://localhost:27017/databasegoeshere' }),
+  store: new MongoStore({ url: 'mongodb://localhost:27017/session' }),
   saveUninitialized: true
 }))
 
@@ -34,7 +36,7 @@ server.use(logger)
 
 server.get('/', function(req, res) {
   userSession = req.session;
-  if (!userSession.email) {
+  if (!userSession.user) {
     res.redirect('/login');
   }
   else {
@@ -48,18 +50,67 @@ server.get('/login', function(req, res) {
 
 server.post('/login', function(req, res){
   userSession = req.session;
-  userSession.email = req.body.emailaddress;
+  mongoClient.connect("mongodb://localhost:27017/logins",function(error,db){
+    if(!error){
+      console.log("Connected successfully to MongoDB server");
+      console.log("LOGIN INFORMATION: "+JSON.stringify(req.body));
+      var collection = db.collection('main');
+      collection.findOne({username:req.body.username.toLowerCase()}, function(error,user){
+          if(user !== null){
+            if(bcrypt.compareSync(req.body.password, user.password)){
+              console.log("Password correct!")
+              userSession.user = user.username;
+              console.log("session user name is "+userSession.user)
+              res.redirect("/")
+              } else {
+              console.log("Login failed! Bad password")
+              res.send(false)
+              }
+          } else { 
+            console.log("Login failed! Bad Username")
+          }
+      });
+    }else{
+      console.dir(error);
+      res.send(error);
+    }
+    db.close();
+  });
+})
+
+server.get('/register', function(req, res) {
+  res.sendfile('client/register.html')
+})
+
+server.post('/register', function(req, res){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  if(!err){
+    mongoClient.connect("mongodb://localhost:27017/logins",function(error,db){
+          if(!error){
+            console.log("Connected successfully to MongoDB server");
+            console.log("LOGIN INFORMATION: "+JSON.stringify(req.body));
+            var collection = db.collection('main');
+            collection.insert({username:req.body.username.toLowerCase(),password:hash})
+          } else{
+            console.dir(error);
+            res.send(error);
+          }
+          db.close();
+      });
+    } else console.log(err)
+  });
   res.redirect('/');
 })
 
 server.get('/logout', function(req, res) {
   req.session.destroy();
+  console.log("session destroyed")
   res.redirect('/')
 })
 
 server.get('/dashboard', function(req, res) {
   userSession = req.session;
-  if (!userSession.email) {
+  if (!userSession.user) {
     res.redirect('/login');
   }
   else {
